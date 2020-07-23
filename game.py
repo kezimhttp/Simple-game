2348548bcbdb16b2b47aa94ab840200368c822e2
KeZiM <kezimoffical@gmail.com>
2020-07-23 02:20:46 +0200

    game.py

---
+++ game.py
@@ -0,0 +1,32 @@
+#Easy game made by kezim
+
+import random
+print("Welcome to the game, it is here for you to guess the drawn number as soon as possible | PRESS ENTER TO NEXT |")
+next = input()
+
+print("Okay! let's go")
+random = random.randint(1,100)
+print("I generated a number in the range 0 100")
+
+tries = 0
+
+x = input()
+x = int()
+
+while random != x:
+    if x < random:
+        tries += 1
+        print("Your number is too small")
+        print("Pls next number")
+        x = input()
+        x = int(x)
+    else:
+        tries += 1
+        print("Your number is too big")
+        print("Pls next number")
+        x = input()
+        x = int(x)
+if random == x:
+    print("You win!")
+    print("Tries:")
+    print(tries)
